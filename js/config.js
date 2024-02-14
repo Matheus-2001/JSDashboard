6f43c100f87c4647f21c7f06e73b12c704c10a3c
Matheus-2001 <theusrodrigues1@hotmail.com>
2024-02-14 18:33:11 -0300

    JSDashboard v-1.0

---
+++ js/config.js
@@ -0,0 +1,49 @@
+// config.js
+//Lista de Progresso 1
+const metaProgressBar2 = 100;
+const realizadoProgressBar2 = 100;
+
+const metaProgressBar3 = 100;
+const realizadoProgressBar3 = 40;
+
+const metaProgressBar4 = 3000;
+const realizadoProgressBar4 = 1500;
+
+const metaProgressBar5 = 100;
+const realizadoProgressBar5 = 100;
+
+const metaProgressBar6 = 100;
+const realizadoProgressBar6 = 20;
+
+
+const metaProgressBars = [metaProgressBar2, metaProgressBar3, metaProgressBar4, metaProgressBar5, metaProgressBar6];
+const realizadoProgressBars = [realizadoProgressBar2, realizadoProgressBar3, realizadoProgressBar4, realizadoProgressBar5, realizadoProgressBar6];
+
+//Lista de Progresso 2
+
+const BmetaProgressBar2 = 100;
+const BrealizadoProgressBar2 = 100;
+
+const BmetaProgressBar3 = 100;
+const BrealizadoProgressBar3 = 100;
+
+const BmetaProgressBar4 = 100;
+const BrealizadoProgressBar4 = 100;
+
+const BmetaProgressBar5 = 100;
+const BrealizadoProgressBar5 = 100;
+
+const BmetaProgressBar6 = 100;
+const BrealizadoProgressBar6 = 100;
+
+
+
+const BmetaProgressBars = [BmetaProgressBar2, BmetaProgressBar3, BmetaProgressBar4, BmetaProgressBar5, BmetaProgressBar6];
+const BrealizadoProgressBars = [BrealizadoProgressBar2, BrealizadoProgressBar3, BrealizadoProgressBar4, BrealizadoProgressBar5, BrealizadoProgressBar6];
+
+
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeuComponente from './componentes/SeuComponente';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
